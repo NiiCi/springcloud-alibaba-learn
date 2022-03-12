a088b867bd9a30f41d59606d227f9593526bb9c4
nichao <1125382627@qq.com>
2022-03-12 22:06:50 +0800

    1. 创建eureka-server-slave服务； 2. 搭建eureka-server集群；

---
+++ cloud-eureka-server-slave/src/main/java/com/niici/springcloud/EurekaSlaveServerApplication.java
@@ -0,0 +1,13 @@
+package com.niici.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaSlaveServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSlaveServerApplication.class, args);
+    }
+}
