280215f9408a76619891cea435af1a652bca46f6
nichao <1125382627@qq.com>
2022-04-21 22:29:37 +0800

    1. 新增gateway路由配置类方式实现路由 -- 缺少filter和predicate；

---
+++ cloud-gateway-gateway/src/main/java/com/niici/springcloud/config/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.niici.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * gateway路由配置类
+ */
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("PAYMENT-SERVICE", route -> route.path("/payment/**").uri("http://localhost:8001"));
+        return routes.build();
+    }
+}
